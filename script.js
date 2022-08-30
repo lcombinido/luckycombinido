1f0c8b1866ad7f983538c15c1fdcf15d2f57f9d8
Lucky Combinido <lucky.combinido@norimaconsulting.com>
2022-08-30 17:54:29 +0800

    desc

--- script.js
+++ script.js
@@ -38,5 +38,15 @@ $(document).ready(function(){
     });
 });
 
+var likeCount = document.querySelector('#likeCount');
+var dislikeCount = document.querySelector('#dislikeCount');
+
+function count(){
+    likeCount.value = parseInt(likeCount.value) + 1;
+}
+
+function count1(){
+    dislikeCount.value = parseInt(dislikeCount.value) + 1;
+} 
 
 
